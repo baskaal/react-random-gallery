f6ca56f211b73797c6a02276e88d7da4f7d2cb87
Bas Kaal <3askaal@gmail.com>
2024-03-13 09:00:00 +0100

    improve preview appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { times, random, last } from 'lodash';
 import randomcolor from 'randomcolor';
-
 import { Gallery, TOptions } from '../lib';
 
-const getNumber = () => {
-  const rangePerBreakpoint: [number, [number, number]][] = [
-    [0, [20, 100]],
-    [320, [40, 140]],
-    [768, [60, 180]],
-    [1024, [80, 220]],
-  ];
+const getNumber = (range: [number, number]) => Math.ceil(random(range[0], range[1]) / 10) * 10;
 
-  const range = last(rangePerBreakpoint.filter(([bp]) => window.innerWidth >= bp))!;
-
-  return Math.ceil(random(range[1][0], range[1][1]) / 10) * 10;
-}
-
-const images = times(25, (index: number) => {
-  const bg = randomcolor().replace('#', '');
-  const w = getNumber();
-  const h = getNumber();
-  const getImgUrl = (retina = 0) =>  `https://placehold.co/${w}x${h}${retina ? `@${retina}x` : ''}/${bg}/fff.webp?font=roboto`;
-
-  return {
-    src: getImgUrl(),
-    srcSet: [getImgUrl(), getImgUrl(2), getImgUrl(3)],
-    alt: `Example image ${index}`
+const getImages = () => {
+  const settingsPerBreakpoint: { [bp: string]: { range: [number, number] } } = {
+    '0': { range: [20, 100] },
+    '320': { range: [40, 140] },
+    '768': { range: [60, 180] },
+    '1024': { range: [80, 220] },
   };
-});
+
+  const settings = last(Object.entries(settingsPerBreakpoint).filter(([bp]) => window.innerWidth >= Number(bp)))![1];
+
+  return times(40, (index: number) => {
+    const bg = randomcolor().replace('#', '');
+    const w = getNumber(settings.range);
+    const h = getNumber(settings.range);
+    const getImgUrl = (retina = 0) =>  `https://placehold.co/${w}x${h}${retina ? `@${retina}x` : ''}/${bg}/fff.webp?font=roboto`;
+
+    return {
+      src: getImgUrl(),
+      srcSet: [getImgUrl(), getImgUrl(2), getImgUrl(3)],
+      alt: `Example image ${index}`
+    };
+  });
+}
 
 const App = () => {
   const options: TOptions = {
@@ -42,6 +41,8 @@ const App = () => {
       duration: '.5s',
     }
   }
+
+  const images = getImages();
 
   return (
     <div style={{ margin: '2rem', border: '1px solid grey' }}>
